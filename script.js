8f233825ac104d8b5bd4620883c9c73b09e2b300
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-01 15:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let MEta = document.getElementById('status');
+let Enter = document.getElementById('enterBtn');
+
+Enter.addEventListener('click', () => {
+	MEta.innerHTML = '<h1>Entered Metaverse</h1>'
+})
